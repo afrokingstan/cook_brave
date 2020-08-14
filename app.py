e5369bee6eab36b05bc9141050f339bbfe8adfa4
afrokingstan <51683427+afrokingstan@users.noreply.github.com>
2020-08-14 18:09:10 +0100

    app.py updated for deployment

--- app.py
+++ app.py
@@ -293,5 +293,5 @@ def OUfilter():
 #end of filter recipe code
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
