c8cedc3af8af2737ce809427e982198b0f819209
afrokingstan <51683427+afrokingstan@users.noreply.github.com>
2020-08-14 07:32:00 +0100

    paginantion feature added and filter code exception added

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ from bson.objectid import ObjectId
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
-
-
-
+# database connection code
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'recipe_manager'
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
@@ -23,8 +21,92 @@ secret = secrets.token_urlsafe(32)
 app.secret_key = secret
 #end of session key generator
 
+#start of exception handling code
+@app.errorhandler(bson.errors.InvalidId)
+def handle_exception(e):
+   flash("That object does not exist",  "danger")
+   return redirect(url_for('get_recipes'))
+
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+   return render_template('500_generic.html'), 500
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template("500_generic.html"), 403
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+#end of exceptopn handling code
+
+#start of all recipe home page code
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("allrecipes.html", recipes=mongo.db.recipes.find().sort("_id", -1))
+
+
+
+
+@app.route('/')
+@app.route('/sort_recipes')
+def sort_recipes():
+    offset = 2
+    limit = 3
+    starting_id = mongo.db.recipes.find().sort("_id", -1)
+    last_id = starting_id[offset]['_id']
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({'_id' : {'$gte' : last_id}}).sort("_id", -1).limit(limit))
+
+#end of all recipes homepage
+
+#start ofpagination code
+@app.route('/oldpagn')
+def oldpagn():
+    flash("old page", "info")
+    offset = 1
+    
+    starting_id = mongo.db.recipes.find().sort("_id", -1)
+    last_id = starting_id[offset]['_id']
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({'_id' : {'$lt' : last_id}}).sort("_id", -1))
+
+
+@app.route('/pagn')
+def pagn():
+    flash("new page", "info")
+    offset = 2
+    limit = 3
+    starting_id = mongo.db.recipes.find().sort("_id", -1)
+    last_id = starting_id[offset]['_id']
+    return render_template("recipes.html", recipes=mongo.db.recipes.find({'_id' : {'$gte' : last_id}}).sort("_id", -1).limit(limit))
+
+#end of pagination
+
+#start of add recipe code
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html',
+                           cuisines=mongo.db.cuisines.find(),
+                           required_tools=mongo.db.required_tools.find())
+
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    flash("You successfully added one recipe", "info")
+    return redirect(url_for('get_recipes'))
+
+#end of add recipe code
+
 #start of file upload file handling 
-##############################################
+@app.route('/uploadshome')
+def uploadshome():
+    flash("Please select a recipe image to upload", "info")
+    return render_template("upload_image.html")
+
 UPLOAD_FOLDER = '/Users/sttec/OneDrive/full stack dev/data project/cook_brave/static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -56,83 +138,7 @@ def upload_image():
             flash('That file extension is not allowed', 'danger')
             return redirect("upload_image.html")
     
-#######################################################################
 #end of file upload file handling
-
-#start of add recipe code
-@app.route('/add_recipe')
-def add_recipe():
-    return render_template('addrecipe.html',
-                           cuisines=mongo.db.cuisines.find(),
-                           required_tools=mongo.db.required_tools.find())
-
-
-@app.route('/insert_recipe', methods=['POST'])
-def insert_recipe():
-    recipes = mongo.db.recipes
-    recipes.insert_one(request.form.to_dict())
-    flash("You successfully added one recipe", "info")
-    return redirect(url_for('get_recipes'))
-
-
-
-#end of add recipe code
-
-
-
-
-
-@app.route('/uploadshome')
-def uploadshome():
-    flash("Please select a recipe image to upload", "info")
-    return render_template("upload_image.html")
-
-
-
-
-
-#start of exception handling code
-
-@app.errorhandler(bson.errors.InvalidId)
-def handle_exception(e):
-   flash("That object does not exist",  "danger")
-   return redirect(url_for('get_recipes'))
-
-
-
-
-@app.errorhandler(Exception)
-def unhandled_exception(e):
-   return render_template('500_generic.html'), 500
-
-@app.errorhandler(403)
-def forbidden(e):
-    return render_template("500_generic.html"), 403
-
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-    
-#end of exceptopn handling code
-
-
-
-
-
-#start of all recipe home page code
-
-@app.route('/')
-@app.route('/get_recipes')
-def get_recipes():
-    return render_template("recipes.html", recipes=mongo.db.recipes.find())
-
-#end of all recipe home page code
-
-
-
 
 # start of edit recipe codes 
 @app.route('/editrecipe_home')
@@ -166,7 +172,8 @@ def update_recipe(recipe_id):
         'date_stamp': request.form.get('date_stamp'),
         'ingredients': request.form.get('ingredients'),
         'image_name': request.form.get('image_name'),
-        'preparation_steps': request.form.get('preparation_steps')
+        'preparation_steps': request.form.get('preparation_steps'),
+        
         
 
     })
@@ -200,50 +207,121 @@ def remove_recipe(recipe_id):
     return redirect(url_for('get_recipes'))
 
 #end of delete recipe code
-############# start of filter recipe code
 
+# start of filter recipe code
 @app.route('/Afrofilter')
 def Afrofilter():
-    flash("These are all the African Cuisine recipes", "info")
-    return render_template("filter.html", recipes=mongo.db.recipes.find({"cuisine_name": "African"}))
+    cur = mongo.db.recipes.find({"required_tools": "African"})
+    results = list(cur) 
+  
+# Checking the cursor is empty 
+# or not 
+    if len(results)==0:
+        flash("These no African recipes, would you like to add one", "info")
+        return render_template("404.html")
+    else:
+        flash("These are all the African Cuisine recipes", "info")
+        return render_template("filter.html", recipes=mongo.db.recipes.find({"cuisine_name": "African"}))
 
 @app.route('/Carifilter')
 def Carifilter():
-    flash("These are all the Caribbean Cuisine recipes", "info")
-    return render_template("filter.html", recipes=mongo.db.recipes.find({"cuisine_name": "Caribbean"}))
+    cur = mongo.db.recipes.find({"required_tools": "Caribbean"})
+    results = list(cur) 
+  
+# Checking the cursor is empty 
+# or not 
+    if len(results)==0:
+        flash("These no Caribbean recipes, would you like to add one", "info")
+        return render_template("404.html")
+    else:
+        flash("These are all the Caribbean Cuisine recipes", "info")
+        return render_template("filter.html", recipes=mongo.db.recipes.find({"cuisine_name": "Caribbean"}))
 
 @app.route('/Spanifilter')
 def Spanifilter():
-    flash("These are all the Spanish recipes", "info")
-    return render_template("filter.html", recipes=mongo.db.recipes.find({"cuisine_name": "Spanish"}))
+    cur = mongo.db.recipes.find({"required_tools": "Spanish"})
+    results = list(cur) 
+  
+# Checking the cursor is empty 
+# or not 
+    if len(results)==0:
+        flash("These no Spanish recipes, would you like to add one", "info")
+        return render_template("404.html")
+    else:
+        flash("These are all the Spanish recipes", "info")
+        return render_template("filter.html", recipes=mongo.db.recipes.find({"cuisine_name": "Spanish"}))
 
 @app.route('/BPfilter')
 def BPfilter():
-    flash("These are all the brand Pressure cooker recipes", "info")
-    return render_template("filter.html", recipes=mongo.db.recipes.find({"required_tools": "brand Pressure cooker"}))
+    cur = mongo.db.recipes.find({"required_tools": "brand Pressure cooker"})
+    results = list(cur) 
+  
+# Checking the cursor is empty 
+# or not 
+    if len(results)==0:
+        flash("These no brand Pressure cooker recipes, would you like to add one", "info")
+        return render_template("404.html")
+    else:
+        flash("These are all the brand Pressure cooker recipes", "info")
+        return render_template("filter.html", recipes=mongo.db.recipes.find({"required_tools": "brand Pressure cooker"}))
 
 @app.route('/BSfilter')
 def BSfilter():
-    flash("These are all the brand steamer recipes", "info")
-    return render_template("filter.html", recipes=mongo.db.recipes.find({"required_tools": "brand steamer"}))
+    cur = mongo.db.recipes.find({"required_tools": "brand steamer"})
+    results = list(cur) 
+  
+# Checking the cursor is empty 
+# or not 
+    if len(results)==0:
+        flash("These no brand steamer recipes, would you like to add one", "info")
+        return render_template("404.html")
+    else:
+        flash("These are all the brand steamer recipes", "info")
+        return render_template("filter.html", recipes=mongo.db.recipes.find({"required_tools": "brand steamer"}))
 
 @app.route('/BGfilter')
 def BGfilter():
-    flash("These are all the brand grill recipes", "info")
-    return render_template("filter.html", recipes=mongo.db.recipes.find({"required_tools": "brand grill"}))
+    cur = mongo.db.recipes.find({"required_tools": "brand grill"})
+    results = list(cur) 
+  
+# Checking the cursor is empty 
+# or not 
+    if len(results)==0:
+        flash("These no the brand grill recipes, would you like to add one", "info")
+        return render_template("404.html")
+    else:
+        flash("These are all the brand grill recipes", "info")
+        return render_template("filter.html", recipes=mongo.db.recipes.find({"required_tools": "brand grill"}))
 
 @app.route('/BCfilter')
 def BCfilter():
-    flash("These are all the brand cooker recipes", "info")
-    return render_template("filter.html", recipes=mongo.db.recipes.find({"required_tools": "brand cooker"}))
+    cur = mongo.db.recipes.find({"required_tools": "brand cooker"})
+    results = list(cur) 
+  
+# Checking the cursor is empty 
+# or not 
+    if len(results)==0:
+        flash("These no the brand cooker recipes, would you like to add one", "info")
+        return render_template("404.html")
+    else:
+        flash("These are all the brand cooker recipes", "info")
+        return render_template("filter.html", recipes=mongo.db.recipes.find({"required_tools": "brand cooker"}))
 
 @app.route('/OUfilter')
 def OUfilter():
-    flash("These are all the others unbranded recipes", "info")
-    return render_template("filter.html", recipes=mongo.db.recipes.find({"required_tools": "others unbranded"}))
+    cur = mongo.db.recipes.find({"required_tools": "others unbranded"})
+    results = list(cur) 
+  
+# Checking the cursor is empty 
+# or not 
+    if len(results)==0:
+        flash("These no others unbranded recipes, would you like to add one", "info")
+        return render_template("404.html")
+    else:
+        flash("These are all the others unbranded recipes", "info")
+        return render_template("filter.html", recipes=mongo.db.recipes.find({"required_tools": "others unbranded"}))
 
-
-################end of filter recipe code
+#end of filter recipe code
 
 
 if __name__ == '__main__':
